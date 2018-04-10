14760b9153f37acafaf8fde0063526479efe401a
Andy K <Dmirtao@users.noreply.github.com>
2018-04-10 16:40:23 -0400

    Created main wrapper and debugging TreeGen

--- main.m
+++ main.m
@@ -2,11 +2,16 @@
 clear; clc; close all;
 
 global bbList;
-BezStructure = struct([]);
+global BezStructure;
 start = [0;0;0];
-maxBranch = 3;
+maxBranch = 2;
 CPorder = 4;
 % 
 aspectRatio = [3;4;1];
-
-TreeGen(start,maxBranch,dist,aspectRatio,CPorder,BezStructure);
+dist = 0;
+samp = 100;
+numBranches = 3;
+BezStructure(1:maxBranch) = struct('curve',[]);
+TreeGen(start,1,maxBranch,dist,aspectRatio,CPorder,BezStructure,samp,numBranches);
+% TreeGen(start,generationIn,maxGeneration,...
+%     dist,aspectRatio,CPorder,inStructure,samples)
