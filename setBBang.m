bac0c2555f5e4d944ba814071dd6c6bf062207d1
Andy K <andykarsai@gmail.com>
2018-04-17 01:52:36 -0400

    Skeleton of setBBang
    See pseudocode, makes transforming bounding box much easier since you're rotating about center of mass

---
+++ setBBang.m
@@ -0,0 +1,9 @@
+function [ output_args ] = setBBang( input_args )
+%SETBBANG Summary of this function goes here
+%   Detailed explanation goes here
+
+% Pseudocode: Translate BB such that midpoint of its diagonal vector is at
+% origin (i.e. [x;y;z] -> [x/2;y/2;z/2]), rotate to desired alignment, then translate back.
+
+end
+
